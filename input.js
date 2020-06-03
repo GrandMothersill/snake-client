c5e823bdb36f6eccf74d65fe8b50d2075865c428
Graham Mothersill <graham.mothersill@gmail.com>
2020-06-03 19:47:07 +0000

    finished snake game

--- input.js
+++ input.js
@@ -1,16 +1,34 @@
 const net = require('net');
 
-const setupInput = function() {
+let connection;
+
+const setupInput = function(conn) {
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
-  stdin.on('data', (key) => {
-    if (key === '\u0003') {
-      process.exit();
-    };
-  });
+  connection = conn;
+  stdin.on('data', (key) => handleUserInput(key, connection));
   return stdin;
 };
 
 module.exports = { setupInput };
+
+
+const handleUserInput = function(key, conn) {
+  if (key === '\u0003') {
+    process.exit();
+  } else if (key === '\u0077') {
+    conn.write('Move: up');
+  } else if (key === '\u0064') {
+    conn.write('Move: right');
+  } else if (key === '\u0073') {
+    conn.write('Move: down');
+  } else if (key === '\u0061') {
+    conn.write('Move: left');
+  } else if (key === '\u0071') {
+    conn.write('Say: AHHHHH');
+  } else if (key === '\u0065') {
+    conn.write('Say: sSsSsS');
+  };
+};
